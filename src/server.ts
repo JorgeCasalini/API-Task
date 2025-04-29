56719a1510b3e59db968ee901f12e777cc73cbba
jorgeCasalini <joshcasalini@gmail.com>
2025-04-29 19:00:06 -0300

    Modify server.ts to include added routes and middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import { router } from "./routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor iniciado na porta http://localhost:${PORT}`))
